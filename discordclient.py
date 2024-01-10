22e5652dc4c74f52075b619625f13f6970f88af0
quylian <90244189+TOA4ASA@users.noreply.github.com>
2024-01-10 13:24:45 +0100

    message reading

--- discordclient.py
+++ discordclient.py
@@ -1,8 +1,10 @@
 # bot.py
 import os
+import random
 
 import discord
 from dotenv import load_dotenv
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -12,5 +14,23 @@ client = discord.Client(intents=discord.Intents.default())
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    # if message.author == client.user:
+    #     return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+    print(message.content)
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
 
 client.run(TOKEN)
